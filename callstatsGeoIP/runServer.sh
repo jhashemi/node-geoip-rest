cc63614f015810646f59b7b078e0f8c506e6be49
Shaohong <shaohong.li@callstats.io>
2015-12-02 11:40:41 +0200

    Something we did for the dockerization effort.

---
+++ callstatsGeoIP/runServer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# download the data files and store them in the node_modules/geoip-lite/data with proper permission
+cd node_modules/geoip-lite/data/;
+
+if [[ "x$GEO_IP_CITY_NAMES_URL" != "x" ]]; then
+    curl -o geoip-city-names.dat ${GEO_IP_CITY_NAMES_URL}
+fi
+
+if [[ "x$GEO_IP_CITY_URL" != "x" ]]; then
+    curl -o geoip-city.dat ${GEO_IP_CITY_URL}
+fi
+
+if [[ "x$GEO_IP_CITY6_URL" != "x" ]]; then
+    curl -o geoip-city6.dat ${GEO_IP_CITY6_URL}
+fi
+
+if [[ "x$GEO_IP_CITY_COUNTRY_URL" != "x" ]]; then
+    curl -o geoip-country.dat ${GEO_IP_CITY_COUNTRY_URL}
+fi
+
+if [[ "x$GEO_IP_CITY_COUNTRY6_URL" != "x" ]]; then
+    curl -o geoip-country6.dat ${GEO_IP_CITY_COUNTRY6_URL}
+fi
+
+if [[ "x$GEO_IP_ISP_URL" != "x" ]]; then
+    curl -o geoip-isp.dat ${GEO_IP_ISP_URL}
+fi
+
+if [[ "x$GEO_IP_ISP6_URL" != "x" ]]; then
+    curl -o geoip-isp6.dat ${GEO_IP_ISP6_URL}
+fi
+
+cd /callstats/geoip
+#start the geoip service
+npm start
